dac18b10bcb61c8a5b0177d4371be51dd19bd0bd
CarlosAcosta4 <carlosacosta4820@gmail.com>
2022-08-10 00:29:12 -0500

    Actualizando archivos

---
+++ Sección_01/proyect_english01.py
@@ -0,0 +1,3 @@
+import os
+os.system('cls')
+print('The name of the beer is:\n' + "'" + input('Enter your favorite color: ') + ' ' + input('Enter your favorite city: ') + "'" + '\n¡Congratulations!')
